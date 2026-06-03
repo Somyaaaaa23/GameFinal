e26ad3b34cb0f07658512d9e89f0a467e991e5a3
Sagar Agarwal <sagaragarwal@Sagars-MacBook-Air-5.local>
2026-06-03 11:11:15 +0530

    error monitoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import React, { Component } from 'react'
+import React from 'react'
 import { Suspense, lazy } from 'react'
 import { AuthProvider, useAuth } from './hooks/useAuth'
 
